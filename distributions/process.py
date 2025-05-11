9130ba74ac2a81d1385dbe8f4263d7525e1e6f8a
Jason R. Coombs <jaraco@jaraco.com>
2025-05-11 14:08:45 -0400

    Implement some smoothing to make the UI less noisy.

--- distributions/process.py
+++ distributions/process.py
@@ -17,4 +17,5 @@ def run():
     res = pypi.Distribution.unprocessed()
     with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
         futures = (executor.submit(dist.process) for dist in res.dists)
-        consume(tqdm.tqdm(concurrent.futures.as_completed(futures), total=res.count))
+        completes = concurrent.futures.as_completed(futures)
+        consume(tqdm.tqdm(completes, total=res.count, smoothing=0.1))
