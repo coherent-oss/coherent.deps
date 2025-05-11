9da6675b780fdca712eed3cbbb1a0457affe9afb
Jason R. Coombs <jaraco@jaraco.com>
2025-05-11 14:15:33 -0400

    Re-configure to cancel futures when an exception including KeyboardInterrupt is encountered.

--- distributions/process.py
+++ distributions/process.py
@@ -15,7 +15,10 @@ from .. import pypi
 @main
 def run():
     res = pypi.Distribution.unprocessed()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
-        futures = (executor.submit(dist.process) for dist in res.dists)
-        completes = concurrent.futures.as_completed(futures)
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=os.cpu_count())
+    futures = (executor.submit(dist.process) for dist in res.dists)
+    completes = concurrent.futures.as_completed(futures)
+    try:
         consume(tqdm.tqdm(completes, total=res.count, smoothing=0.1))
+    finally:
+        executor.shutdown(wait=True, cancel_futures=True)
