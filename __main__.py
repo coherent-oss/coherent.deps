a585c5891e6c13b99a662da5bfa172c8479e4744
Jason R. Coombs <jaraco@jaraco.com>
2025-05-17 15:04:01 -0400

    Use emit to yield lines, and render the lines in one place.

--- __main__.py
+++ __main__.py
@@ -15,31 +15,31 @@ import glob
 import pathlib
 
 from jaraco.ui.main import main
-from more_itertools import flatten
+from more_itertools import consume, flatten
 
 from . import imports, pypi
 
 
 def emit_plain(deps):
-    print(*deps, sep="\n")
+    yield from deps
 
 
 def emit_toml(deps):
-    print("[project]\nrequires = [")
-    print(*map(lambda d: f'    "{d}",', deps), sep="\n")
-    print("]")
+    yield "[project]\nrequires = ["
+    yield from map(lambda d: f'    "{d}",', deps)
+    yield "]"
 
 
 def emit_inline(deps):
-    print("""# ///\n# requires-python = ">=3.8"\n# dependencies = [""")
-    print(*map(lambda d: f'#     "{d}",', deps), sep="\n")
-    print("# ]\n# ///")
+    yield """# ///\n# requires-python = ">=3.8"\n# dependencies = ["""
+    yield from map(lambda d: f'#     "{d}",', deps)
+    yield "# ]\n# ///"
 
 
 def emit_python(deps):
-    print("""__requires__ = [""")
-    print(*map(lambda d: f'    "{d}",', deps), sep="\n")
-    print("]")
+    yield """__requires__ = ["""
+    yield from map(lambda d: f'    "{d}",', deps)
+    yield "]"
 
 
 def parse_glob(spec: str):
@@ -54,4 +54,5 @@ def main(
     files = flatten(map(parse_glob, globs))
     imps = flatten(map(imports.get_module_imports, files))
     deps = (pypi.distribution_for(imp) for imp in imps if not imp.excluded())
-    globals()[f'emit_{format}'](deps)
+    lines = globals()[f'emit_{format}'](deps)
+    consume(map(print, lines))
